5ddd6f902d388bb87b28ab84bfbb63834c88ba56
naoisemcgarrigle <nmgarr@outlook.com>
2025-11-22 22:35:16 +0000

    Commit 7 (Webhook test 1)

--- src/main/java/dev/naoisemcg/Main.java
+++ src/main/java/dev/naoisemcg/Main.java
@@ -3,5 +3,7 @@ package dev.naoisemcg;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("####\n ## \n####");
+        System.out.println("WEBHOOK TEST.......");
     }
 }
